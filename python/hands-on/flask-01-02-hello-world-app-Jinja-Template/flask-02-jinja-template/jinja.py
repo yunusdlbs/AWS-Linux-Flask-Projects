b210ab7bcd3e934ae464d24d8260a81bb86b153e
Yunus <yunusdelibas@outlook.com>
2022-01-18 23:32:45 +0300

    flask-01 and flask-02 added to GitHub repo...

---
+++ python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-02-jinja-template/jinja.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app= Flask(__name__)
+
+@app.route('/')
+def head():
+    return render_template('index.html', number1 = 2397, number2 = 3847)
+
+@app.route('/mult')
+def number():
+    var1, var2 = 3, 70
+    return render_template('body.html', num1 = var1, num2 = var2, multiplication = var1*var2)
+
+
+if __name__=='__main__':
+    app.run(debug=True)
