c20f06a7e54ccc7a3bfb1af2c14100d005ee4d11
Yunus <yunusdelibas@outlook.com>
2022-03-21 21:53:37 +0300

    I added my new hands-on sheets and changed some info in .env and settings.py...

--- AWS/Capstone-Project-Blog-Page-App-(Django)-on-AWS-Environment/src/cblog/settings.py
+++ AWS/Capstone-Project-Blog-Page-App-(Django)-on-AWS-Environment/src/cblog/settings.py
@@ -87,9 +87,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'capstonedb', # database name in RDS is written here
         'USER': 'admin', # database master username in RDS is written here
-        'PASSWORD': config('2005Maltepe'),
-        'HOST': '',  # database endpoint is written here
-        'PORT': '' # database port is written here
+        'PASSWORD': config('PASSWORD'),
+        'HOST': 'capstone-database.clxvqduqmbww.us-east-1.rds.amazonaws.com',  # database endpoint is written here
+        'PORT': '3306' # database port is written here
     }
 }
 
@@ -143,9 +143,9 @@ LOGIN_REDIRECT_URL = "blog:list"
 LOGIN_URL = "login"
 
 
-AWS_STORAGE_BUCKET_NAME = '' # please enter your s3 bucket name
+AWS_STORAGE_BUCKET_NAME = 'capstone-media-yunus' # please enter your s3 bucket name
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_REGION_NAME = "" # please enter your s3 region 
+AWS_S3_REGION_NAME = "us-east-1" # please enter your s3 region 
 AWS_DEFAULT_ACL = 'public-read'
 
 AWS_LOCATION = 'static'
