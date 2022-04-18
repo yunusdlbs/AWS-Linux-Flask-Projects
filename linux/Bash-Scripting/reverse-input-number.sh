6d759b814be0801f33bafe3e229e8bd5b34f2af8
Yunus <yunusdelibas@outlook.com>
2022-04-18 05:11:22 +0300

    Created bash-scripting folder and added files.

---
+++ linux/Bash-Scripting/reverse-input-number.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+read -p "Enter a number: " num
+temp=$num
+while [ $temp -ne 0 ]
+do
+    reverse=$reverse$((temp%10))
+    temp=$((temp/10))
+done
+echo "Reverse of $num is $reverse"
+
+# Other solutions:
+
+# read -p "Enter your number: " num
+# echo "reverse of $num is: "
+# echo $num | rev
+
+# echo enter n
+# read n
+# num=0
+# while [ $n -gt 0 ]
+# do
+# num=$(expr $num \* 10)
+# k=$(expr $n % 10)
+# num=$(expr $num + $k)
+# n=$(expr $n / 10)
+# done
+# echo number is $num  
