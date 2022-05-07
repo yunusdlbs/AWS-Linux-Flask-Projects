d8d104222bf273200c24040b2a74574c070df9e9
Yunus <yunusdelibas@outlook.com>
2022-05-08 01:58:35 +0300

    edited some parts of fibonacci_numbers.sh

--- linux/Bash-Scripting/fibonacci_numbers.sh
+++ linux/Bash-Scripting/fibonacci_numbers.sh
@@ -1,17 +1,18 @@
 #! /bin/bash
-# Write a shell script about Fibonacci Numbers...
+# Write a bash script about Fibonacci Numbers...
 a=0
 b=1
 
 read -p "Enter your index number for Fibonacci Numbers: " n
 counter=2
-echo $a  
-echo $b
-while [[ $counter -le $n ]]; do
+echo -n "[ $a $b "  
 
+while [[ $counter -le $n ]]; do
 counter=`expr $counter+1`
 fib=`expr $a + $b`
 a=$b
 b=$fib
-echo $fib 
+echo -n "$fib " 
 done
+
+echo -n "]"
