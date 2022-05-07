16711c76861df469a36ce8fb389d67ae8aeb7198
Yunus <yunusdelibas@outlook.com>
2022-05-07 22:21:12 +0300

    fibonacci_numbers.sh added to repo....

---
+++ linux/Bash-Scripting/fibonacci_numbers.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+a=0
+b=1
+
+read -p "Enter your index number for Fibonacci Numbers: " n
+counter=2
+echo $a  
+echo $b
+while [[ $counter -le $n ]]; do
+
+counter=`expr $counter+1`
+fib=`expr $a + $b`
+a=$b
+b=$fib
+echo $fib 
+done
